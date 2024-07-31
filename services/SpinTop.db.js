38386e430e9ec335290dae2d55772f70017b4c69
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-07-31 14:31:15 -0230

    commit dal_layer

--- services/SpinTop.db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "kelly",
-  host: "localhost",
-  database: "SpinTop",
-  password: "KeyinCollege",
-  port: 5432,
-});
-module.exports = pool;
