a847db54b4665eddbdea30b00d4f4431bbd67e13
joy111joy <mitchel_joy@hotmail.com>
2024-08-02 01:53:24 -0230

    Added index web structure and styling

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const session = require("express-session");
+const apiRouter = require("./routes/api/index");
 const myEventEmitter = require("./services/logEvents.js");
 
-// Import Routers
-const searchRouter = require("./routes/search");
-const authRouter = require("./routes/auth");
-const apiRouter = require("./routes/api");
+app.use("/api", apiRouter);
 
 const PORT = process.env.PORT || 3000;
 global.DEBUG = true;
@@ -18,19 +16,14 @@ global.DEBUG = true;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || "default_secret",
+    secret:
+      process.env.SESSION_SECRET || " kstoyles@cluster0.ufay3sq.mongodb.net",
     resave: false,
     saveUninitialized: false,
   })
 );
-
-// Use Routers
-app.use("/search", searchRouter);
-app.use("/auth", authRouter);
-app.use("/api", apiRouter);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
@@ -38,7 +31,7 @@ app.listen(PORT, (err) => {
     "event",
     "app.listen",
     "SUCCESS",
-    "HTTP search site successfully started."
+    "http search site successfully started."
   );
   console.log(`Simple app running on port ${PORT}.`);
 });
@@ -48,7 +41,7 @@ app.get("/", async (req, res) => {
     "event",
     "app.get",
     "INFO",
-    "Landing page (index.ejs) was displayed."
+    "landing page (index.ejs) was displayed."
   );
   res.render("index", { status: req.session.status });
 });
@@ -58,12 +51,17 @@ app.get("/about", async (req, res) => {
     "event",
     "app.get /about",
     "INFO",
-    "About page (about.ejs) was displayed."
+    "about page (about.ejs) was displayed."
   );
   res.render("about", { status: req.session.status });
 });
 
-// 404 Error Page
+const searchRouter = require("./routes/search");
+app.use("/search", searchRouter);
+
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
+
 app.use((req, res) => {
   res.status(404).render("404", { status: req.session.status });
 });
